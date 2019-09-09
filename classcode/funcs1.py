4ba077284e330ed5a337a20395646875dd239c6d
Mike Zamansky <zamansky@gmail.com>
2019-09-09 09:05:34 -0400

    added funcs1

---
+++ classcode/funcs1.py
@@ -0,0 +1,26 @@
+def f(a):
+    a = a + 10
+    return a
+
+def inc(val):
+    val = val + 1
+    return val
+
+def add2(a,b):
+    result = a + b
+    return result
+
+a = 10
+print(f(a))
+b = 10
+c = inc(b)
+print("C is ",c)
+
+a = 15
+b = 30
+result = add2(a,b)
+print(result)
+
+s1 = "hello"
+s2 = "world"
+print(add2(s1,s2))
--- classcode/intro2.py
+++ classcode/intro2.py
@@ -1,13 +1,24 @@
 
 def f(a):
-    return a+1
+    a = a + 10
+    return a
 
-
-
-num = 20;
+num = 20
 print(num)
 print(f(num))
 print(num)
 x = f(num)
-num= f(num)
+num = f(num)
 print(x,num)
+
+z = 10
+print(z)
+z = z + 5
+print(z)
+
+
+a = 30
+print(a)
+x = f(a)
+print(a,x)
+
