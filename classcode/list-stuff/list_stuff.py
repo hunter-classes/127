2f23740b9cfc6a31574e959d7ad67955730cdbb3
Mike Zamansky <zamansky@gmail.com>
2019-10-07 09:07:36 -0400

    added starter for mode

--- classcode/list-stuff/list_stuff.py
+++ classcode/list-stuff/list_stuff.py
@@ -12,6 +12,14 @@ def freqency(x,l):
 
 
 def find_largest(l):
+    """
+    inputs: a list of numbers
+    returns: the largest values
+    >>> find_largest([5,3,2,6,10,1,3])
+    10
+    >>> find_largest([2,2,2,2,2,10,10,10,11,11,11])
+    11
+    """
     largest = l[0]
     for item in l:
         if item > largest:
@@ -35,3 +43,19 @@ def make_random_list(num_items,max_size):
     return result
 
 l = make_random_list(20,20)
+
+
+def mode(l):
+    """
+    Return the value of the  mode (most frequent value) from list l
+    assume that there is only one mode (if there are multiple modes
+    you can return any of them)
+
+    >>> mode([5,4,2,6,2,9,6,8,6,10])
+    6
+    >>> mode([1,2,3,4,5,4,6,7,5,10,11,12])
+    4
+    >>> mode([1,2,3,4,5,6,7,8])
+    1
+    """
+    pass
