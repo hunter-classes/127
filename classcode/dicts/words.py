299b7685986e61eb9e33c99a1749d85f8b6d3264
Mike Zamansky <zamansky@gmail.com>
2019-11-04 09:24:04 -0500

    finished words.py

--- classcode/dicts/words.py
+++ classcode/dicts/words.py
@@ -1,6 +1,6 @@
 
 # open the file
-f = open("chapter1.txt")
+f = open("mobydick.txt")
 
 # read the entire text into a big string
 #all = f.read()
@@ -17,11 +17,43 @@ f = open("chapter1.txt")
 
 all = f.read()
 cleaned = all.lower()
-for letter_to_remove in ",.;!?-_0123456789":
-    cleaned = cleaned.replace(letter_to_remove,'')
+#for letter_to_remove in ",.;!?-_0123456789":
+#    cleaned = cleaned.replace(letter_to_remove,'')
+
+letters_to_remove = ',.;!?-_0123456789'
+cleaned = [ x for x in cleaned if x not in letters_to_remove]
+cleaned = "".join(cleaned)
+
+
+# word_counts = {}
+# for word in cleaned.split():
+#     word_counts.setdefault(word,0)
+#     word_counts [word] = word_counts[word] + 1
 
 word_counts = {}
-for word in cleaned.split():
+wordlist = cleaned.split()
+for index in range(len(wordlist)):
+    word = wordlist[index]
     word_counts.setdefault(word,0)
-    word_counts [word] = word_counts[word] + 1
-    
+    word_counts[word] = word_counts[word] + 1
+
+values = word_counts.values()
+values.sort()
+
+over50 = [(word,word_counts[word]) \
+          for word in word_counts \
+          if word_counts[word] > 50]
+
+from20to50 = []
+for word in word_counts:
+    if word_counts[word] >= 20 and word_counts[word] <= 50:
+        from20to50.append( (word,word_counts[word]))
+        
+from10to20 = [(word,word_counts[word]) \
+              for word in word_counts \
+              if word_counts[word] > 10 and word_counts[word] < 20]
+
+u10 = [(word,word_counts[word]) \
+              for word in word_counts \
+              if word_counts[word] <10]
+
