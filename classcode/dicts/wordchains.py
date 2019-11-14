c6b83bfb62bdcfd5d660c8a9dc86b5a0a2b58cf5
Mike Zamansky <zamansky@gmail.com>
2019-11-14 08:28:54 -0500

    can now build wordchains of arbitrary prefix length

--- classcode/dicts/wordchains.py
+++ classcode/dicts/wordchains.py
@@ -1,6 +1,59 @@
 import random
+import sys
 
-def build_wordchain(filename):
+def build_wordchain3(filename,prefix_length):
+    f = open(filename)
+    rawdata = f.read()
+
+    # clean the data
+    rawdata = rawdata.lower()
+    letters_to_keep = 'abcdefghijklmnopqrstuvwxyz \t\n'
+    cleaned = [ x for x in rawdata if x in letters_to_keep]
+ 
+    cleaned = "".join(cleaned)
+
+    wordlist = cleaned.split()
+    d={}
+
+    for i in range(len(wordlist)-prefix_length):
+        #prefix or key
+        prefix = tuple(wordlist[i:i+prefix_length])
+
+        #suffix or value
+        suffix = wordlist[i+prefix_length]
+
+        print(prefix,suffix)
+        d.setdefault(prefix,[])
+        d[prefix].append(suffix)
+    return d
+
+
+def build_wordchain2(filename):
+    f = open(filename)
+    rawdata = f.read()
+
+    # clean the data
+    rawdata = rawdata.lower()
+    letters_to_keep = 'abcdefghijklmnopqrstuvwxyz \t\n'
+    cleaned = [ x for x in rawdata if x in letters_to_keep]
+ 
+    cleaned = "".join(cleaned)
+
+    wordlist = cleaned.split()
+    d={}
+
+    for i in range(len(wordlist)-2):
+        w1 = wordlist[i]
+        w2 = wordlist[i+1]
+        w3 = wordlist[i+2]
+        key = (w1,w2)
+        val = w3
+        d.setdefault(key,[])
+        d[key].append(val)
+
+    return d
+
+def build_wordchain1(filename):
     f = open(filename)
     rawdata = f.read()
 
@@ -32,22 +85,24 @@ def generate(d,numwords,startword):
     story = " ".join(result)
     return story
 
-#result = build_wordchain("testset.txt")
-words = build_wordchain("psalms.txt")
+words = build_wordchain3("testset.txt",1)
+#words = build_wordchain2("testset.txt")
+#words = build_wordchain("psalms.txt")
 
-tuple_dict = { ("when","shall") : ["we"],
-               ("shall","we") : ["meet"],
-               ("we","meet") : ["again"],
-               ("meet","again") : ["in"]}
-twl = "we meet happily".split()
-k = (twl[0],twl[1])
-v = twl[2]
-tuple_dict[k].append(v)
+print(words)
+# tuple_dict = { ("when","shall") : ["we"],
+#                ("shall","we") : ["meet"],
+#                ("we","meet") : ["again"],
+#                ("meet","again") : ["in"]}
+# twl = "we meet happily".split()
+# k = (twl[0],twl[1])
+# v = twl[2]
+# tuple_dict[k].append(v)
 
-for k in tuple_dict:
-    #w1 = k[0]
-    #w2 = k[1]
-    (w1,w2) = k
-    v = tuple_dict[k]
-    print(w1,w2,v)
+# for k in tuple_dict:
+#     #w1 = k[0]
+#     #w2 = k[1]
+#     (w1,w2) = k
+#     v = tuple_dict[k]
+#     print(w1,w2,v)
     
