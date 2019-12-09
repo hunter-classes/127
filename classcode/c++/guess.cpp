61b21434525f9bd62109cd73d6ce1836e8f87adc
Mike Zamansky <zamansky@gmail.com>
2019-12-09 09:10:39 -0500

    addd guess sample

--- classcode/c++/guess.cpp
+++ classcode/c++/guess.cpp
@@ -11,6 +11,13 @@ int main()
       2. get if it's too high, low or right
       3. update guess if necessary
   */
-  
-  return 0;
+
+
+  /* version 2 - a separate program named guess2.cpp
+     computer stores a number
+     User will enter an initial guess
+     Loop until user guesser right
+       1. get new user guess
+       2. tell user if it's too high low or right
+     return 0;
 }
