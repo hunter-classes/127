058b31dc8711f655f9390cf1a701f2131c4f106c
Mike Zamansky <zamansky@gmail.com>
2020-11-16 09:16:39 -0500

    updated all

--- classcode/ii/ii.py
+++ classcode/ii/ii.py
@@ -3,23 +3,32 @@ def clean(text):
     """
     clean the text - turn all lower case, remove unwanted chars
     """
-    return text
+    letters_to_keep="abcdefghijklmnopqrstuvwxyz \t\n"
+    text = text.lower()
+    cleaned = [x for x in text if x in letters_to_keep]
+    return "".join(cleaned)
 
-
+ 
 def add_text_to_index(index, document, text):
     # go through the text a word a a time
     # and for each word add the  document to index entry
     # that has the word as the key
+    for word in text.split():
+        index.setdefault(word,[])
+        if document not in index[word]:
+            index[word].append(document)
     return index
 
 def build_index_from_file(filename):
-    reader = csv.DictReader(open(filename))
+    reader = csv.DictReader(open(filename,encoding='Latin-1'))
     index = {}
+    people = {}
     for line in reader:
-        document = line['document']
-        text = line['text']
+        document = line['TDCJNumber']
+        people[document] = line
+        text = line['LastStatement']
         text = clean(text)
-        index = add_text_to_index(index,document,text)
-    return index
+        index = add_text_to_index(index, document, text)
+    return (people,index)
 
 
