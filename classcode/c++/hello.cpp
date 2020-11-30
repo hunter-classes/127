bb48517fd8af256b5a18395f15befb37c60eca7b
Mike Zamansky <zamansky@gmail.com>
2020-11-30 08:58:08 -0500

    added hello.cpp

---
+++ classcode/c++/hello.cpp
@@ -0,0 +1,24 @@
+#include <iostream> // kindof like import in python but different
+
+
+// this is a single line comment
+
+/*
+  this is a multi
+  line comment 
+*/
+
+
+
+// this is a a function in c++
+// every c++ function NEEDS a funciton named main
+// it is automatically run when you run your program.
+int main()
+{
+
+  std::cout << "Hello World!" << std::endl;
+
+  std::cout << "This is a second line with an embedded newline\n";
+  
+  return 0;
+}
