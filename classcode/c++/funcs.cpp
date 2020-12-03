84dbb1136bb5ef8445be937199b464a2c9a2c0c6
Mike Zamansky <zamansky@gmail.com>
2020-12-03 08:34:58 -0500

    added void func

--- classcode/c++/funcs.cpp
+++ classcode/c++/funcs.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 
+
+/*
+  variables are available in the scope (function) in which they
+  are declared. You can use the same name in different scopes (but
+  be careful about that).
+
+  Parameters are passed value <-- the parameter gets a COPY of the value
+  from the caller. If the parameter is changed in the function it is
+  only the copy that's changed. The variable in the caller is
+  not affected.
+
+*/
 int add_two_numbers(int a, int b){
   int c;
   c = a + b;
   a = 5000;
   b = 3000;
   
-
+  std::cout << "add func: "<< a << " , " << b << " , " << c << "\n";
   return c;
 
+}
+
+void printstuff(){
+  std::cout << "this function just prints stuff\n";
+  return; // this returns immediately. The last line won't be executed
+  std::cout << "It doesn't return any values <-- we use void for that\n";
+  
 }
 
 int main()
@@ -30,8 +49,10 @@ int main()
 
   c = add_two_numbers(a,b);
 
-  std::cout << a << " , " << b << " , " << c << "\n";
+  std::cout << "Main: " << a << " , " << b << " , " << c << "\n";
 
+  printstuff();
+  
   return 0;
 }
     
