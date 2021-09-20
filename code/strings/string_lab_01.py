01d2859602f67d789b8afa50b5d96cee22470525
Mike Zamansky <zamansky@gmail.com>
2021-09-20 10:23:59 -0400

    added string lab

---
+++ code/strings/string_lab_01.py
@@ -0,0 +1,31 @@
+
+def initialize(name):
+    """
+    takes a name in the form "First Last" and returns "F. Last"
+    ex: initializa("Mike Zamansky") would return "M. Zamansky"
+    pass
+
+def capBoth(name):
+    """
+    takes a name in the form "first last" and returns the  name
+    capitalized.
+    ex: capBoth("mike zamansky") --> "Mike Zamansky"
+    pass
+
+def bondify(name):
+    """
+    takes a name in the form "first last" and returns 
+    "last, first last"
+    ex: bondify("James Bond") --> "Bond, James Bond"
+    """
+    pass
+
+def tests():
+    # add tests here
+    pass
+
+
+
+if _name__=="__main__":
+    tests()
+    
