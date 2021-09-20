0e1e430a44171e85de69b033f57a2e242d7deaa6
Mike Zamansky <zamansky@gmail.com>
2021-09-20 10:13:08 -0400

    added string func

--- code/misc_hw/ch7_9.py
+++ code/misc_hw/ch7_9.py
@@ -11,6 +11,37 @@ def isEvenTerse(number):
 def isOdd(number):
     return not isEven(number)
 
+longString ="""
+Street bands have been picking up the slack for the last year in NYC, performing for outside diners and more throughout the city. This festival celebrates them and puts them front and center.
+
+Email readers can see the video 
+"""
+def countLetters(s,letter):
+    """ 
+    count the number of times letter occurs in string s
+    """
+    count = 0
+    for l in s:
+        print(l)
+
+
+longString ="""
+Street bands have been picking up the slack for the last year in NYC, performing for outside diners and more throughout the city. This festival celebrates them and puts them front and center.
+
+Email readers can see the video 
+"""
+def countLetters(s,letter):
+    """ 
+    count the number of times letter occurs in string s
+    """
+    count = 0
+    print(s)
+    for l in s:
+        if l==letter.lower() or l==letter.upper():
+            count = count + 1
+    return count 
+
+
 def tests():
     print("IsEven tests")
     print("isEven(23)",isEven(23))
@@ -24,7 +55,8 @@ def tests():
     print("isOdd(23)",isOdd(23))
     print("isOdd(24)",isOdd(24))
     print()
-
+    print("Count letters a in longString")
+    print(countLetters(longString,'A'))
 
 if __name__=="__main__":
     # put the stuff here to be run automatically if you
