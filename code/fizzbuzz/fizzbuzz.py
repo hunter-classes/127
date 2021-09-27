f92bdbafaebbdd1a6b6105cea86d5fdcde4d827c
Mike Zamansky <zamansky@gmail.com>
2021-09-27 10:09:16 -0400

    added string building version

--- code/fizzbuzz/fizzbuzz.py
+++ code/fizzbuzz/fizzbuzz.py
@@ -1,12 +1,25 @@
+def fb1():
+    for i in range(1,100):
+        if i % 3 == 0 and i % 5 == 0:
+            print(i,"fizzbuzz")
+        elif i % 3 == 0:
+            print(i,"fizz")
+        elif i % 5 == 0:
+            print(i,"buzz")
+        else:
+            print(i)
 
-for i in range(1,100):
-    if i % 3 == 0 and i % 5 == 0:
-       print("fizzbuzz")
-    elif i % 3 == 0:
-        print("fizz")
-    elif i % 5 == 0:
-        print("buzz")
-    else:
-       print(i)
 
-    
+def fb2():
+
+    for i in range(1,100):
+        output = ""
+        if i % 3 == 0:
+            output = output + "fizz"
+        if i % 5 == 0:
+            output = output + "buzz"
+        if output == "":
+            output = i
+        print(i,output)
+
+fb2()
