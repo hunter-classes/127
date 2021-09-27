430d055064a86ca1fe73c8b169352dc5bd14d39f
Mike Zamansky <zamansky@gmail.com>
2021-09-27 10:04:43 -0400

    added fizzbuzz

---
+++ code/fizzbuzz/fizzbuzz.py
@@ -0,0 +1,12 @@
+
+for i in range(1,100):
+    if i % 3 == 0 and i % 5 == 0:
+       print("fizzbuzz")
+    elif i % 3 == 0:
+        print("fizz")
+    elif i % 5 == 0:
+        print("buzz")
+    else:
+       print(i)
+
+    
