8a0beaf910eb9db767d9a3bb606edcdf8b9dfdfc
Mike Zamansky <zamansky@gmail.com>
2021-10-04 10:02:45 -0400

    added hw-06

---
+++ code/lists/hw-06.py
@@ -0,0 +1,40 @@
+import random
+
+def build_random_list(size,minval,maxval):
+    l = []
+    for i in range(size):
+        l.append(random.randrange(minval,maxval))
+    return l
+
+def find_min(l):
+    if (len(l)<1):
+        return None
+    smallest_so_far = l[0]
+    for item in l:
+        if item < smallest_so_far:
+            smallest_so_far = item
+    return smallest_so_far
+
+def combine(l1,l2):
+    len1 = len(l1)
+    len2 = len(l2)
+    if len1 < len2:
+        shorter = len1
+    else:
+        shorter = len2
+    result = []
+    i = 0
+    while i < shorter:
+        result.append(l1[i] + l2[i])
+        i = i + 1
+    if shorter == len1:
+        result = result + l2[i:]
+    else:
+        result = result + l1[i:]
+    return result
+        
+def list_sum(l):
+    pass
+
+def sub_sum(l):
+    pass
