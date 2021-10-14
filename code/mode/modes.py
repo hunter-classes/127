f31f44d58651257306c0264bd525379b757219bc
Mike Zamansky <zamansky@gmail.com>
2021-10-14 10:50:02 -0400

    got mode working

--- code/mode/modes.py
+++ code/mode/modes.py
@@ -1,9 +1,29 @@
+import random
 
- 
+def frequencies(l,value):
+    count = 0;
+    for item in l:
+        if item == value:
+            count = count + 1
+    return count
+
 
 def mode(L):
-    pass
+    # assume the first one is the mode so far
+    mode_so_far = L[0]
+    count_so_far = frequencies(L,mode_so_far)
+    
+    # for each item,
+    #   count how many times it appears
+    #   if it appears more than the mode so far
+    #   it becomes the new mode so far
+    for value in L:
+        count_value = frequencies(L,value)
+        if count_value > count_so_far:
+            count_so_far = count_value
+            mode_so_far = value
 
+    return mode_so_far
 def main():
     pass
 
