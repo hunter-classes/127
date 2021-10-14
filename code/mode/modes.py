75dcafb3cd3eff434cfb92084dcab7e52044275f
Mike Zamansky <zamansky@gmail.com>
2021-10-14 10:55:40 -0400

    got mode working with count

--- code/mode/modes.py
+++ code/mode/modes.py
@@ -24,6 +24,24 @@ def mode(L):
             mode_so_far = value
 
     return mode_so_far
+
+def mode2(L):
+    # assume the first one is the mode so far
+    mode_so_far = L[0]
+    count_so_far = L.count(mode_so_far)
+    
+    # for each item,
+    #   count how many times it appears
+    #   if it appears more than the mode so far
+    #   it becomes the new mode so far
+    for value in L:
+        count_value = L.count(value)
+        if count_value > count_so_far:
+            count_so_far = count_value
+            mode_so_far = value
+
+    return mode_so_far
+
 def main():
     pass
 
