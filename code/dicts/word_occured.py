a82220211d5c846131513107f6410a2be7dba892
Mike Zamansky <zamansky@gmail.com>
2021-11-01 10:21:25 -0400

    added word_occured

--- code/dicts/word_occured.py
+++ code/dicts/word_occured.py
@@ -15,14 +15,17 @@ def build_word_counts(text):
     for word in text.split():
         word_counts.setdefault(word,0)
         word_counts[word] = word_counts[word] + 1
-    
     return word_counts
 
-#v = letter_counts.values()
-#m = find_max(list(v))
-#largest_values = [x for x in letter_counts.keys() if letter_counts[x] == m]
+def get_most_frequent_word(text):
+    counts = build_word_counts(text)
+    vals = counts.values()
+    maxval = find_max(list(vals))
 
-
+    largest_values = [x for x in counts.keys() if counts[x] == maxval]
+    return largest_values
 
 text = open("input.txt").read()
-counts = build_word_counts(text)
+result = get_most_frequent_word(text)
+print(result)
+
