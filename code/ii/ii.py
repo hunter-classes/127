c5dcd28da714894f7c01318d1055cdbaf1502aec
Mike Zamansky <zamansky@gmail.com>
2021-11-29 10:45:32 -0500

    have a working index builder

--- code/ii/ii.py
+++ code/ii/ii.py
@@ -16,6 +16,21 @@ def clean(line):
     cleaned = "".join(result)
     return cleaned
     
+def remove_stop_words(line):
+    f = open("stopwords.txt")
+    stopwords = f.read().split()
+    result = [x for x in line.split() if x not in stopwords]
+    line = " ".join(result)
+    return line
+
+def add_statement(index,key,statement):
+    for word in statement.split():
+        index.setdefault(word,[])
+        if key not in index[word]:
+            index[word].append(key)
+    return index
+
+
 
 f = open("last_statements_20.csv", encoding='Latin-1')
 
@@ -28,5 +43,10 @@ for item in reader:
     key = item['TDCJNumber']
     people[key] = item
     statement = clean(item['LastStatement'])
+
+    # we might not want to remove the stop words
+    statement = remove_stop_words(statement)
     index = add_statement(index,key,statement)
-    print()
+
+for key  in index.keys():
+    print(key,len(index[key]))
