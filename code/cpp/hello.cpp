1502188abeabd88e6fe4561e0717c1d4bcf37d94
Mike Zamansky <zamansky@gmail.com>
2021-11-30 10:55:41 -0500

    added simple c++ demo

---
+++ code/cpp/hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+  int a,b;
+  int c;
+  
+  a = 20;
+  b = 30;
+  c = a + b;
+  std::cout << "Hello World!" << std::endl;
+  std::cout << a << " + " << b << " = " << c << "\n";
+  return 0;
+}
---
+++ code/cpp/hello.py
@@ -0,0 +1,6 @@
+a = 20
+b = 30
+c = a + b;
+
+print("Hello World!")
+print(c)
