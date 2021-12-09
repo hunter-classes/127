335be26ef829f290f3190f2afdeb86bbf7b4ce3e
Mike Zamansky <zamansky@gmail.com>
2021-12-09 10:52:41 -0500

    added all the function examples

--- code/c++/funcs.cpp
+++ code/c++/funcs.cpp
@@ -13,15 +13,35 @@ void personal_greeter(std::string name){
   std::cout << "Hello " << name << "!" << "\n";
 }
 
-/*
-  def greet(name):
-  print("hello",name)
-*/
+// using a return type - this returns one more than x (increment)
+int inc(int x){
+  int b = x + 1;
+  return b;
+}
+
+// force an integer division on two doubles
+// note this is returning an int
+int int_div(double a, double b){
+  int result;
+  result = int(a) / int(b);
+  return result;
+}
 
 int main()
 {
   greet();
   personal_greeter("Thomas");
+  int input = 20;
+  int output;
+  output = inc(input);
+  std::cout << output << " is one more than " << input << "!\n";
+
+  double a = 50.3;
+  double b = 25.5;
+  int intdiv = int_div(a,b);
+  std::cout << a << "/" << b << " is " << a/b << "\n";
+  std::cout << a << " int_div " << b << " is " << intdiv << "\n";
+  
   return 0;
 }
 
