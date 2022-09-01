1d2cbffaa6dbae7b8ab3a8d25b0f5a6edbaab6fa
Mike Zamansky <zamansky@gmail.com>
2022-09-01 09:19:02 -0400

    added triangle

--- code/turtles/t2.py
+++ code/turtles/t2.py
@@ -7,15 +7,29 @@ crush = turtle.Turtle()
 # draw a square
 crush.forward(50)
 crush.right(90)
-
+crush.forward(50)
+crush.right(90)
+crush.forward(50)
+crush.right(90)
 crush.forward(50)
 crush.right(90)
 
-crush.forward(50)
-crush.right(90)
+squirt = turtle.Turtle()
 
-crush.forward(50)
-crush.right(90)
+# draw a triangle
+squirt.up()
+squirt.goto(100,100)
+squirt.down()
+squirt.color("red")
+squirt.width(5)
+squirt.forward(50)
+squirt.right(120)
+squirt.forward(50)
+squirt.right(120)
+squirt.forward(50)
+squirt.right(120)
+
+
 
 wn.exitonclick()
 wn.mainloop()
