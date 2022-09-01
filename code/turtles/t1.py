02cebbbf46b27d3118f56ce8d6f8fd26a7c2d5b8
Mike Zamansky <zamansky@gmail.com>
2022-09-01 12:47:14 +0000

    added t1.py

---
+++ code/turtles/t1.py
@@ -0,0 +1,16 @@
+import turtle
+
+wn = turtle.Screen()
+
+crush = turtle.Turtle()
+
+crush.forward(50)
+crush.left(30)
+crush.forward(130)
+crush.right(50)
+crush.forward(70)
+
+wn.exitonclick()
+wn.mainloop()
+
+a = 10 + 30 
