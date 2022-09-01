97d1337f0cf254773f1791f909d726f9968f6190
Mike Zamansky <zamansky@gmail.com>
2022-09-01 08:58:31 -0400

    uploaded assignment

---
+++ code/turtles/t2.py
@@ -0,0 +1,21 @@
+import turtle
+
+wn = turtle.Screen()
+
+crush = turtle.Turtle()
+
+# draw a square
+crush.forward(50)
+crush.right(90)
+
+crush.forward(50)
+crush.right(90)
+
+crush.forward(50)
+crush.right(90)
+
+crush.forward(50)
+crush.right(90)
+
+wn.exitonclick()
+wn.mainloop()
