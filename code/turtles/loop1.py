18a13a5c2ea181c3aafe15550e770a3b62435ea7
Mike Zamansky <zamansky@gmail.com>
2022-09-01 09:31:27 -0400

    added loop sample

---
+++ code/turtles/loop1.py
@@ -0,0 +1,14 @@
+
+for i in range(4):
+    print(i)
+ 
+print("--------------------") 
+for number in range(5,10):
+    print(number)
+    
+print("--------------------")
+for name in ['able','baker','charlie']:
+    print(name)
+    print(name.upper())
+
+print("The loop is done")
