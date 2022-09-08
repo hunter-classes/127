7fcc82da2d4b2bafaf6ed7c564ca0942c8c89ee3
Mike Zamansky <zamansky@gmail.com>
2022-09-08 09:41:55 -0400

    added triangle

--- code/turtles/turtles-functions.py
+++ code/turtles/turtles-functions.py
@@ -24,8 +24,30 @@ def square(t,x,y,w,color,sidelen):
         t.forward(sidelen)
         t.right(90)
 
-#def triangle(fill in these):
-#    #code to draw the triangle
+def triangle(t,x,y,w,color,sidelen):
+    """
+    Draw a triangle using the turtle passed into t
+
+    Parameters:
+      t       - a turtle
+      x,y     - location
+      w       - width of side
+      color   - color to draw in
+      sidelen - length of each side
+    Returns:
+      nothing
+    """
+    # set the location, color, and width
+    t.penup()
+    t.goto(x,y)
+    t.width(w)
+    t.color(color)
+    t.pendown()
+    # draw a square
+    for i in range(3):
+        t.forward(sidelen)
+        t.right(120)
+
 
 #def hexagon(fill in these):
 #    #code to draw the hexagon
@@ -44,5 +66,7 @@ square(squirt,100,100,5,"red",80)
 square(crush,-50,30,3,"yellow",100)
 crush.setheading(45)
 square(crush,150,30,2,"blue",60)
-wn.exitonclick()
+
+triangle(crush,-100,-50,2,"green",50)
+
 wn.mainloop()
