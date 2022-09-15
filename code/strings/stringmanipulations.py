cd12ded260e78fc6754635c5f29205472a081f6e
Mike Zamansky <zamansky@gmail.com>
2022-09-15 09:17:13 -0400

    added string manipulations

---
+++ code/strings/stringmanipulations.py
@@ -0,0 +1,12 @@
+def initialize(name):
+    """
+    input: a string in the form "first last"
+    returns: a string in the form "F. Last"
+    """
+    
+def bondify(name):
+    """
+    input: a string in the form "first last"
+    returns: a string in the form "Last, First Last"
+    """
+    
