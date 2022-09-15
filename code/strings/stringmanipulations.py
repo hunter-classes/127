fba0d2a9cd0314b451b83a422662e1d9246a0c20
Mike Zamansky <zamansky@gmail.com>
2022-09-15 09:38:22 -0400

    added initialize code

--- code/strings/stringmanipulations.py
+++ code/strings/stringmanipulations.py
@@ -3,6 +3,18 @@ def initialize(name):
     input: a string in the form "first last"
     returns: a string in the form "F. Last"
     """
+    result = ""
+    # isolate, uppercase and add first init to result
+    first = name[0]
+    first = first.upper()
+    result = result + first + "."
+
+    # find the last name (after space), cap it and add to result
+    location = name.find(' ')
+    last = name[location+1:].capitalize()
+    result = result + ' ' + last
+    return result
+    
     
 def bondify(name):
     """
@@ -10,3 +22,9 @@ def bondify(name):
     returns: a string in the form "Last, First Last"
     """
     
+# Test initialize
+result = initialize("james bond")
+print("james bond --> ",result)
+
+
+# Test Bondify
