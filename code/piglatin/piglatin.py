db233cb45e6e68428ad2f93fa2ccbd4fa0cf69d6
Mike Zamansky <zamansky@gmail.com>
2022-09-22 08:46:42 -0400

    got it working, first cut

--- code/piglatin/piglatin.py
+++ code/piglatin/piglatin.py
@@ -1,12 +1,21 @@
-# piglatin
 def piglatinify(word):
-    """
-    convert word to piglatin
-    """
-    return word
+    
+    first = word[0]
+    if first == 'a' or first == 'e' or first == 'i' \
+               or first == 'o' or first == 'u':
+        result = word + 'ay'
+    else:
+        # move first letter to end and add 'ay'
+        result = word[1:]+first+'ay'
+    
+    return result
 
 
 # Testing
 test_word = "hello"
+result = piglatinify(test_word)
+print(test_word," -> ",result)
+
+test_word = "able"
 result = piglatinify(test_word)
 print(test_word," -> ",result)
