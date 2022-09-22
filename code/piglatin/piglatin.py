aa82b361d4015562b03eec0db606daad84e44f8a
Mike Zamansky <zamansky@gmail.com>
2022-09-22 09:00:09 -0400

    added todo items

--- code/piglatin/piglatin.py
+++ code/piglatin/piglatin.py
@@ -1,3 +1,11 @@
+# TODO:
+# 1. Make it work for capitalized words
+#    ex: Cable -> Ablecay
+# 2. Handle periods (and possibly other punctuation)
+#    ex: Able. -> Ableay.
+#        cable. -> ablecay.
+
+
 def piglatinify(word):
     
     first = word[0]
@@ -16,5 +24,17 @@ result = piglatinify(test_word)
 print(test_word," -> ",result)
 
 test_word = "able"
+result = piglatinify(test_word)
+print(test_word," -> ",result)
+
+test_word = "Cable"
+result = piglatinify(test_word)
+print(test_word," -> ",result)
+
+test_word = "Able."
+result = piglatinify(test_word)
+print(test_word," -> ",result)
+
+test_word = "cable."
 result = piglatinify(test_word)
 print(test_word," -> ",result)
