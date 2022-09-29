c106bdf94c87b9546c78ad839535709828b44e02
Mike Zamansky <zamansky@gmail.com>
2022-09-29 08:42:14 -0400

    finished fizzbuzz

--- code/fizzbuzz/fizzbuzz.py
+++ code/fizzbuzz/fizzbuzz.py
@@ -1,7 +1,19 @@
 
 def fizzbuzz(n):
-    pass
-
+#    number = 1
+#    while number < n:
+#        print(number)
+#        number = number + 1
+    for number in range(1,n):
+        if number % 3 == 0 and number % 5 == 0:
+            print("fizzbuzz")
+        elif number % 3 == 0:
+            print("fizz")
+        elif number % 5 == 0:
+            print("buzz")
+        else:
+            print(number)
+        
 
 value = 20
 print("Fizzbuzz up to ",value)
