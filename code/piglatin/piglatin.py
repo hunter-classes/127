fae0a459c8c46b3a97d1e00cb5cf26489b08f0bd
Mike Zamansky <zamansky@gmail.com>
2022-09-29 09:19:45 -0400

    added v2 of dealing with capitalized words

--- code/piglatin/piglatin.py
+++ code/piglatin/piglatin.py
@@ -6,19 +6,44 @@
 #        cable. -> ablecay.
 
 
-def piglatinify(word):
+def piglatinify_v1(word):
     
     first = word[0]
     if first in 'aeiouAEIOU':
         result = word + 'ay'
     else:
-        # move first letter to end and add 'ay'
-        # check to see if it's upper case
         if first == first.upper():
             result = word[1:].capitalize()+first.lower()+'ay'
         else:
             result = word[1:]+first+'ay'
     
+    return result
+
+
+def piglatinify(word):
+    
+    # keep track of if the word had a capital letter
+    if word[0] == word[0].upper():
+        capital = True
+    else:
+        capital = False
+    
+    # transform to lower case
+    word = word[0].lower()+word[1:]
+    first = word[0]
+
+    # turn into piglatin
+    if first in 'aeiou':
+        result = word + 'ay'
+    else:
+        result = word[1:]+first+'ay'
+    
+    # if we started with a capital letter we
+    # have to transform the result back to have
+    # a capital letter
+    if capital:
+        result = result.capitalize()
+        
     return result
 
 
@@ -35,10 +60,18 @@ test_word = "Cable"
 result = piglatinify(test_word)
 print(test_word," -> ",result)
 
+test_word = "Able"
+result = piglatinify(test_word)
+print(test_word," -> ",result)
+
 test_word = "Able."
 result = piglatinify(test_word)
 print(test_word," -> ",result)
 
 test_word = "cable."
+result = piglatinify(test_word)
+print(test_word," -> ",result)
+
+test_word = "deGromm"
 result = piglatinify(test_word)
 print(test_word," -> ",result)
