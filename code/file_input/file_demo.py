f56442c9ad7b5560bdcd72ee1058fd95826738ad
Mike Zamansky <zamansky@gmail.com>
2022-10-17 09:04:01 -0400

    added read and readline demo

--- code/file_input/file_demo.py
+++ code/file_input/file_demo.py
@@ -1,1 +1,21 @@
-    
+
+# read in the entire file
+# as a big string of characters
+# f = open("data.dat")
+# data = f.read() 
+
+# each time we do f.readline()
+# we read in the next line 
+# line = f.readline()
+# print(line)
+# f.seek(0)
+# line = f.readline()
+# print(line)
+
+
+f = open('data.dat')
+data = f.read()
+
+f.close()
+f = open('data.dat')
+lines = f.readlines()
