f6a97c17ac9126373257911910587743faa0f4f2
Mike Zamansky <zamansky@gmail.com>
2022-10-20 08:49:28 -0400

    added list examples

---
+++ code/lists/advanced_lists.py
@@ -0,0 +1,21 @@
+import random
+l1 = [1,2,3,5]
+s = 'hello world'
+number = 20
+
+def make_list(item):
+    return [item]
+
+list_of_tens = []
+for i in range(5):
+    list_of_tens.append(10)
+
+counting_list = []
+for i in range(10):
+    counting_list = counting_list + [i]
+
+random_list = []
+for i in range(20):
+    random_list.append(random.randrange(5,15))
+    
+
