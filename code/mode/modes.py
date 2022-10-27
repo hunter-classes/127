78c2386b4fdc2faa35581dfb2fee984b9fa99ce9
Mike Zamansky <zamansky@gmail.com>
2022-10-27 09:27:50 -0400

    added first cut of mode

--- code/mode/modes.py
+++ code/mode/modes.py
@@ -1,3 +1,4 @@
+import datetime
 import random
 
 def findLargest(dataset):
@@ -37,8 +38,25 @@ def mode(dataset):
     Ex: mode([5,4,5,6,7,8,5,4]) --> 5 since 5 appears the most
     mode([5,5,5,4,4,4,2,2,7,7,8,8,9]) --> return 5 or 4 since
     both of those values appear 3 times which is the most
+
+    Strategy:
+    assume the first value is the mode
+    we can grab its frequency
+
+    for each remaining item in the dataset:
+      count that items frequence and see if it's the new
+      mode so far    
     """
-    pass
+    modeSoFar = dataset[0]
+    freqSoFar = freq(dataset,modeSoFar)
+    for item in dataset[1:]:
+        if freq(dataset,item) > freqSoFar:
+            modeSoFar = item
+            freqSoFar = freq(dataset,item)
+            
+    return modeSoFar
+
+
 
 
 dataset = buildRandomList(20,30)
