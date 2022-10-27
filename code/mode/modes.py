5de1730e4033206e112d36d7928ac3c25b5867ca
Mike Zamansky <zamansky@gmail.com>
2022-10-27 08:58:59 -0400

    added mode stub

--- code/mode/modes.py
+++ code/mode/modes.py
@@ -10,12 +10,12 @@ def findLargest(dataset):
 
 
 def freq(dataset,v):
-    count = 0
-    for item in dataset:
-        if item == v:
-            count = count + 1
-    return count
-
+    #count = 0
+    #for item in dataset:
+    #    if item == v:
+    #        count = count + 1
+    #return count
+    return len([x for x in dataset if x == v])
 
 def buildRandomList(size,maxvalue):
     #result = []
@@ -24,5 +24,21 @@ def buildRandomList(size,maxvalue):
     #return result
     result = [random.randrange(maxvalue) for x in range(size)]
     return result 
-                      
+
+
+def mode(dataset):
+    """
+    Returns a mode of the dataset, that is
+    the value that appears most frequently
+
+    if multiple values appear the same # of times and are
+    most frequent, return any of them.
+
+    Ex: mode([5,4,5,6,7,8,5,4]) --> 5 since 5 appears the most
+    mode([5,5,5,4,4,4,2,2,7,7,8,8,9]) --> return 5 or 4 since
+    both of those values appear 3 times which is the most
+    """
+    pass
+
+
 dataset = buildRandomList(20,30)
