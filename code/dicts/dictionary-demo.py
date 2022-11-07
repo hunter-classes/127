12671fab08d48945fc1f0a47cab52bc42c28344f
Mike Zamansky <zamansky@gmail.com>
2022-11-07 09:02:53 -0500

    added more dict examples including tuples

--- code/dicts/dictionary-demo.py
+++ code/dicts/dictionary-demo.py
@@ -40,3 +40,14 @@ for k,v in person.items():
 
 for k in person.keys():
     print(k,person[k])
+
+s1 = {'gender':'m','name':'barney','id':1111,'scores':[100,90,85]}
+s2 = {'gender':'f','name':'jen','id':4444,'scores':[95,85]}
+s3 = {'gender':'m','name':'marcel','id':3333,'scores':[99,76,88,82]}
+s4 = {'gender':'f','name':'sookie','id':2222,'scores':[98]}
+
+student_list = [s1,s2,s3,s4]
+student_dict = {}
+for item in student_list:
+    student_dict[item['id']] = item
+    
