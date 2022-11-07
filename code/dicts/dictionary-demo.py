0dc7a507c7987d7730224c7d0611b124cdbca955
Mike Zamansky <zamansky@gmail.com>
2022-11-07 08:53:00 -0500

    added more dict examples including tuples

--- code/dicts/dictionary-demo.py
+++ code/dicts/dictionary-demo.py
@@ -11,7 +11,32 @@ print(d)
 
 person = {'fist' : "John",
           'last' : "Smith",
-          'age' : 50}
+          'age' : 50,
+          'height' : 60}
 person['age'] = person['age'] + 1
 
 print(person)
+
+k = person.keys()
+print(k)
+for item in k:
+    print('person[',item,'] = ',person[item])
+
+# convert the dict_keys thing into a list:
+klist = [x for x in person.keys()]
+print(klist)
+
+# pull out the values and convert to list
+vlist = [x for x in person.values()]
+
+
+xl = [3,5]
+xt = (3,5)
+yt = ('hello',44,34.5)
+a,b,c = yt
+
+for k,v in person.items():
+    print(k,v)
+
+for k in person.keys():
+    print(k,person[k])
