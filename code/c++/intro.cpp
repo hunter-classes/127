e475b676138a9e36e9e635915d02eb18c3a031c6
Mike Zamansky <zamansky@gmail.com>
2022-11-28 09:27:31 -0500

    added intro c++ program

---
+++ code/c++/intro.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+// this is a single line comment
+
+/*
+  This is a multi line
+  comment
+
+  to compile:
+  g++ intro.cpp
+  or
+  g++ -o intor intro.cpp
+
+  to run:
+  ./a.out
+  or
+  ./intro.cpp
+  
+  
+ */
+
+int main()  // here to end of line is a comment 
+{
+  std::cout << "Hello World!" << std::endl;
+  
+  return 0;
+}
